875aca21e2b68f541e216c45bc925ae0e8b18d1b
BereanK <bereankibet@gmail.com>
2026-08-03 14:55:20 +0300

    Bug fix

--- app/Console/Commands/DevCommand.php
+++ app/Console/Commands/DevCommand.php
@@ -25,7 +25,7 @@ class DevCommand extends Command
         $user = Auth::loginUsingId(12);
 
         $logbookWithoutTransferFee = LogbookProfile::where('LogBookFee', '<=', 0)
-            ->where('CreatedOn','>=', now()->subMonths(8))
+            // ->where('CreatedOn','>=', now()->subMonths(8))
             ->get();
 
 
